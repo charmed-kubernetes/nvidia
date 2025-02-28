dc5c0b51c01a43fadb3ffa2849f06e7a99c6d31e
Adam Dyess <adam.dyess@canonical.com>
2025-02-28 12:19:42 -0600

    feat: Update to v24.9.2 from v23.9.0

--- charms/gpu-operator/upstream/update.sh
+++ charms/gpu-operator/upstream/update.sh
@@ -11,7 +11,7 @@ BASE_DIR=$(dirname ${BASH_SOURCE[0]})
 CHART_DIR=${BASE_DIR}/charts
 CHART_NAME=gpu-operator
 CHART_REPO=https://helm.ngc.nvidia.com/nvidia/charts
-CHART_VER=${1:-v23.9.0}
+CHART_VER=${1:-v24.9.2}
 MANIFEST_DIR=${BASE_DIR}/${CHART_NAME}
 MANIFEST_VER=v${CHART_VER//v}
 TEMPLATE_PATH=${MANIFEST_DIR}/manifests/${MANIFEST_VER}/manifest.yaml
@@ -26,8 +26,7 @@ helm fetch ${CHART_REPO}/${CHART_NAME}-${CHART_VER}.tgz \
 mkdir -p $(dirname -- ${TEMPLATE_PATH})
 echo ${MANIFEST_VER} > ${MANIFEST_DIR}/version
 helm template ${TEMPLATE_RELEASE} ${CHART_DIR}/${CHART_NAME}-${CHART_VER}.tgz \
-  --include-crds \
-  > ${TEMPLATE_PATH}
+  --include-crds | tee ${TEMPLATE_PATH}
 
 # We've tainted the chart; drop helm-isms to avoid confusion
 sed -i \
